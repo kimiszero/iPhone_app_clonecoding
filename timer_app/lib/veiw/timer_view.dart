e741b0945d30bdd9905f3a1499af189b6cec5ea1
kimeunyeong <zerokim0215@gmail.com>
2023-04-05 00:10:18 +0900

    add: alram button

--- timer_app/lib/veiw/timer_view.dart
+++ timer_app/lib/veiw/timer_view.dart
@@ -32,29 +32,63 @@ class _TimerViewState extends State<TimerView> {
                   radius: 40,
                   backgroundColor: Colors.grey.shade900,
                   child: CupertinoButton(
-                      child: Text('취소', style: TextStyle(color: Colors.black54),), onPressed: (){}),
+                      child: Text(
+                        '취소',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {}),
                 ),
                 CircleAvatar(
                   radius: 40,
                   backgroundColor: Colors.green.shade700..opacity,
                   child: CupertinoButton(
-                      child: Text('시작', style: TextStyle(color: Colors.green.shade900),), onPressed: (){}),
+                      child: Text(
+                        '시작',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {}),
                 ),
-
               ],
-            )
+            ),
+            const SizedBox(height: 50),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.white12,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              padding: EdgeInsets.only(left: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('타이머 종료 시', style: TextStyle(color: Colors.white, fontSize: 17),),
+                  CupertinoButton(
+                      child: Row(
+                        children: const [
+                          Text(
+                            '전파 탐지기',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          SizedBox(width: 5),
+                          Icon(
+                            Icons.chevron_right,
+                            color: Colors.white,
+                          ),
+                        ],
+                      ),
+                      onPressed: () {}),
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
 
-  Widget renderTimer(){
+  Widget renderTimer() {
     return Center(
       child: CupertinoTheme(
-        data: CupertinoThemeData(
-            brightness: Brightness.dark
-        ),
+        data: CupertinoThemeData(brightness: Brightness.dark),
         child: CupertinoTimerPicker(
           mode: CupertinoTimerPickerMode.hms,
           onTimerDurationChanged: (Duration newDuration) {},
