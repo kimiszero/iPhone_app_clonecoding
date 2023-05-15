17daf3c69870cb1f9ac7b0bd3592e0e8f2432eb5
kimeunyeong <zerokim0215@gmail.com>
2023-05-15 23:58:03 +0900

    add: 사칙연산 로직 추가

--- calculator/lib/main.dart
+++ calculator/lib/main.dart
@@ -21,13 +21,18 @@ class Calculator extends StatefulWidget {
 }
 
 class _CalculatorState extends State<Calculator> {
-  List<dynamic> item = [
-    0, '.', '=', 1, 2, 3, '+', 4, 5, 6, '-', 7, 8, 9, 'x', 'AC', '+/-', '%', '➗'
-  ];
+  List<String> nums=[];
+  int first_num=0;
+  int last_num=0;
+  String operator='';
+  int before=0;
+  int result=0;
+  bool isTap=false;
 
   @override
   void initState() {
     super.initState();
+    nums=[];
   }
 
   @override
@@ -42,7 +47,7 @@ class _CalculatorState extends State<Calculator> {
                 children: [
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 20.0),
-                    child: Text('0', style: TextStyle(color: Colors.black, fontSize: 100, fontWeight: FontWeight.w300),),
+                    child: Text(isTap==false?first_num.toString():result.toString(), style: TextStyle(color: Colors.black, fontSize: 100, fontWeight: FontWeight.w300),),
                   ),
                 ],
               ),
@@ -56,14 +61,73 @@ class _CalculatorState extends State<Calculator> {
     );
   }
 
+  void onTapOperator(String name){
+    if(first_num.isNaN){
+      String first='';
+      for(int i=0; i<nums.length; i++){
+        first+=nums[i];
+      }
+      first_num=int.parse(first);
+    }
+
+    nums=[];
+    operator=name;
+    isTap=true;
+    setState(() {});
+  }
+
+  void onTapAssign(){
+
+    String res='';
+    for(int i=0; i<nums.length; i++){
+      res+=nums[i];
+    }
+    last_num=int.parse(res);
+
+    if(operator=='+'){
+      result=first_num + last_num;
+    }else if(operator=='-'){
+      result=first_num-last_num;
+    }else if(operator=='x'){
+      result=first_num*last_num;
+    }else if(operator=='÷'){
+      result=first_num~/last_num;
+    }
+
+    first_num=result;
+    operator='';
+    last_num=0;
+    nums=[];
+    setState(() {});
+  }
+
+  void onTapNumber(String num){
+    print('onTapNumber');
+    nums.add(num);
+    print('nums $nums');
+
+    setState(() {});
+  }
+
+  void onTapAc(){
+    // number=0;
+    first_num=0;
+    nums=[];
+    before=0;
+    result=0;
+    operator='';
+    isTap=false;
+    setState(() {});
+  }
+
   Widget _acRowButtonWidgets(){
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _intButtonWidget('ac', 90, Colors.grey.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('ac', 90, Colors.grey.withOpacity(0.7), ()=>onTapAc(), Colors.white),
         _intButtonWidget('+/- ', 90, Colors.grey.withOpacity(0.7), (){}, Colors.white),
         _intButtonWidget('%', 90, Colors.grey.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('÷', 90, Colors.amber.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('÷', 90, Colors.amber.withOpacity(0.7), ()=>onTapOperator('÷'), Colors.white),
       ],
     );
   }
@@ -72,10 +136,10 @@ class _CalculatorState extends State<Calculator> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _intButtonWidget('7', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('8', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('9', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('x', 90, Colors.amber.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('7', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('7'), Colors.white),
+        _intButtonWidget('8', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('8'), Colors.white),
+        _intButtonWidget('9', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('9'), Colors.white),
+        _intButtonWidget('x', 90, Colors.amber.withOpacity(0.7), ()=>onTapOperator('x'), Colors.white),
       ],
     );
   }
@@ -84,10 +148,10 @@ class _CalculatorState extends State<Calculator> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _intButtonWidget('4', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('5', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('6', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('-', 90, Colors.amber.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('4', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('4'), Colors.white),
+        _intButtonWidget('5', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('5'), Colors.white),
+        _intButtonWidget('6', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('6'), Colors.white),
+        _intButtonWidget('-', 90, Colors.amber.withOpacity(0.7), ()=>onTapOperator('-'), Colors.white),
       ],
     );
   }
@@ -96,10 +160,10 @@ class _CalculatorState extends State<Calculator> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _intButtonWidget('1', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('2', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('3', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('+', 90, Colors.amber.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('1', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('1'), Colors.white),
+        _intButtonWidget('2', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('2'), Colors.white),
+        _intButtonWidget('3', 90, Colors.black87.withOpacity(0.7), ()=>onTapNumber('3'), Colors.white),
+        _intButtonWidget('+', 90, Colors.amber.withOpacity(0.7), ()=>onTapOperator('+'), Colors.white),
       ],
     );
   }
@@ -108,16 +172,16 @@ class _CalculatorState extends State<Calculator> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceAround,
       children: [
-        _intButtonWidget('0', 200, Colors.black87.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('0', 200, Colors.black87.withOpacity(0.7), ()=>onTapNumber('0'), Colors.white),
         _intButtonWidget('.', 90, Colors.black87.withOpacity(0.7), (){}, Colors.white),
-        _intButtonWidget('=', 90, Colors.amber.withOpacity(0.7), (){}, Colors.white),
+        _intButtonWidget('=', 90, Colors.amber.withOpacity(0.7), ()=>onTapAssign(), Colors.white),
       ],
     );
   }
 
-  Widget _intButtonWidget(String name, double width, Color color, Function onTap, Color textColor,) {
+  Widget _intButtonWidget(String name, double width, Color color, void Function() onTap, Color textColor,) {
     return InkWell(
-      onTap: onTap(),
+      onTap: onTap,
       child: Container(
         margin: EdgeInsets.all(5),
         padding: EdgeInsets.all(5),
